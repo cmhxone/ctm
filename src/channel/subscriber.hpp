b7bd7cf7b0b0e2aa60b393ff8ae196638fb9f9d9
조무현 <cmhxone@outlook.kr>
2024-12-02 15:00:39 +0900

    주석 추가

--- src/channel/subscriber.hpp
+++ src/channel/subscriber.hpp
@@ -6,8 +6,17 @@
 #include "./event/event.hpp"
 
 namespace channel {
+/**
+ * @brief 채널 구독자 추상 클래스
+ *
+ */
 class Subscriber {
 public:
+  /**
+   * @brief 이벤트 핸들링 추상 메소드
+   *
+   * @param event
+   */
   virtual void handleEvent(const event::Event *event) = 0;
 
 protected:
