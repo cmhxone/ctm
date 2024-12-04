d07566a55874faf175136c73a987c5995df4ae65
조무현 <cmhxone@outlook.kr>
2024-12-04 17:48:39 +0900

    변수명 중복 선언 오타 수정

--- src/ctm/bridge/message_bridge.hpp
+++ src/ctm/bridge/message_bridge.hpp
@@ -39,23 +39,23 @@ public:
     switch (event->getEventType()) {
       // CTI 메시지는 파싱하여 클라이언트들에게 던져준다
     case channel::event::EventType::CTI_EVENT: {
-      const channel::event::CTIEvent *event =
+      const channel::event::CTIEvent *cti_event =
           dynamic_cast<const channel::event::CTIEvent *>(event);
 
-      switch (event->getMessageType()) {
+      switch (cti_event->getMessageType()) {
       // Heartbeat 응답
       case cisco::common::MessageType::HEARTBEAT_CONF: {
 
         const cisco::session::HeartbeatConf heart_beat_conf =
             cisco::common::deserialize<cisco::session::HeartbeatConf>(
-                event->getPacket());
+                cti_event->getPacket());
         spdlog::debug("Heartbeat conf received. invoke_id: {}",
                       heart_beat_conf.getInvokeID());
       } break;
       default:
         spdlog::debug(
             "CTI_Event received. (non-handled message type) message_type: {}",
-            static_cast<std::int32_t>(event->getMessageType()));
+            static_cast<std::int32_t>(cti_event->getMessageType()));
         break;
       }
 
